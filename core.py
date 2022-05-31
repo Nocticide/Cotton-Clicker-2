f08e7df23da54f3734d30350918ee44d8137e715
/nocticide <72424187+Nocticide@users.noreply.github.com>
2022-05-30 23:22:56 -0400

    Update and rename core.txt to core.py

---
+++ core.py
@@ -0,0 +1,38 @@
+Let's make a game!
+  name:Cotton Clicker 2
+  by:Nocticide
+  desc:rest in piece my man cotton clicker 1
+
+Resources
+  *Cotton
+    name:Cotton
+    desc:Rich, luscious, the gold of the natural world.
+
+Upgrades
+  *applePieRecipe 
+    name:Apple pie recipe
+    desc:Unlocks the ability to make apple pies!
+    
+  *cherryPieRecipe
+    name:Cherry pie recipe
+    desc:Boy, you'll never guess!
+
+Buildings
+  *brownCow
+    name:Brown cow
+    desc:A big friendly cow that generates 1 milk per second.   
+    cost:20 coins
+    on tick:give 1 milk
+
+Buttons
+ *pickCotton
+  name:Pick cotton
+  desc:Click to pick cotton. Gains 1 cotton.
+  on click:yield 1 Cotton
+    
+Shinies
+ #*cottonClicky
+ # name:Cotton Plant
+ # on click:yield (random(10,100)) gold
+ # movement:anywhere fade bounce moveRandom
+ # frequency:120
